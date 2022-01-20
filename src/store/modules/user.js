9460f1bcc1302962426aaeea41ea305d8ee4823f
ZHOUZH1CH3N <zzc1393925530@gmail.com>
2022-01-20 15:01:54 +0800

    feat(响应数据): 统一处理后台返回数据

--- src/store/modules/user.js
+++ src/store/modules/user.js
@@ -1,10 +1,19 @@
 import md5 from 'md5'
 import { login } from '../../api/sys'
+import { setItem, getItem } from '../../utils/storage'
+import { TOKEN } from '@/constant'
 
 export default {
   namespaced: true,
-  state: () => ({}),
-  mutations: {},
+  state: () => ({
+    token: getItem(TOKEN) || ''
+  }),
+  mutations: {
+    setToken(state, token) {
+      state.token = token
+      setItem(TOKEN, token)
+    }
+  },
   actions: {
     login(context, userInfo) {
       const { username, password } = userInfo
@@ -14,6 +23,7 @@ export default {
           password: md5(password)
         })
           .then((data) => {
+            this.commit('user/setToken', data.token)
             resolve(data)
           })
           .catch((err) => {
