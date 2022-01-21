cb507b00290e5c45d1313712fa0cf06f8dfe3320
ZHOUZH1CH3N <zzc1393925530@gmail.com>
2022-01-21 11:13:37 +0800

    feat: 渲染用户头像菜单

--- src/store/getters.js
+++ src/store/getters.js
@@ -4,6 +4,7 @@ const getters = {
   // return true表示用户信息已经存在
   hasUserInfo: (state) => {
     return JSON.stringify(state.user.userInfo) !== '{}'
-  }
+  },
+  userInfo: (state) => state.user.userInfo
 }
 export default getters
